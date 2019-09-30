d0f96126478e97dc109972d348e31f0a1a5440ba
unknown <yousef.rz@gmail.com>
2019-09-29 23:17:58 -0400

    code added

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
+#Number of rows to skip from the top (the older data)
+toprow = 7000
 #read the data into a DataFrame
-sp500_data = pd.read_csv('sp500.csv', index_col='Date', parse_dates=True)
-print(sp500_data.head())
+sp500_data = pd.read_csv('sp500.csv', index_col='Date', parse_dates=True, skiprows=[i for i in range(1,toprow+1)])
+
+#print the last 5 rows in the DataFrame and its number of rows and columns
+print(sp500_data.tail())
+print(sp500_data.shape)
+
 #plot the closing value of the index against date
 plt.figure(figsize=(14,6))
 plt.xlabel("Date")
