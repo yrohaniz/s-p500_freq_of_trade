a62ae8a9fd115b25ecea6880b4547f4431d7cddd
unknown <yousef.rz@gmail.com>
2019-10-08 17:42:46 -0400

    new changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
+import strategy as stg
 
 #Number of rows to skip from the top (the older data)
 toprow = int(input("Please specify the number of days from the past you would like to skip? "))
@@ -39,27 +40,29 @@ trade_dollars = 0.0
 adj_closing_val_list = sp500_data['Adj Close']
 
 #Calculate and initialize the trading dollars per unit of the index
-val_per_index = dollars/adj_closing_val_list[1]  #the invested dollars per unit index
-trade_d_per_index = trade_dollars/adj_closing_val_list[1]  #the dollars in the trade bin per unit index
-max_trade = assumed_trade_units/adj_closing_val_list[1]  #maximum value of traded dollars per unit index
+val_per_index = dollars/adj_closing_val_list[2]  #the invested dollars per unit index
+trade_d_per_index = trade_dollars/adj_closing_val_list[2]  #the dollars in the trade bin per unit index
+max_trade = assumed_trade_units/adj_closing_val_list[2]  #maximum value of traded dollars per unit index
 
 tot_num_trades = 0
 #Perform the trade strategy
-for i in range(number_rows-1):
-    slope = adj_closing_val_list[i+1] - adj_closing_val_list[i]  #Calculate the diffence between the closing values of the index for today and yesterday
-    if slope > 0 and trade_d_per_index >= max_trade:  #If the slope is positive and there are enough dollars for trade, buy
+for i in range(number_rows-2):
+    #Calculate the diffence between the closing values of the index for today and yesterday, and yesterday and the day before it
+    slope_a = adj_closing_val_list[i+1] - adj_closing_val_list[i] 
+    slope_b = adj_closing_val_list[i+2] - adj_closing_val_list[i+1]
+    if (slope_b > 0 and slope_a < 0) and trade_d_per_index >= max_trade:  #If local min and there are enough dollars for trade, buy
         trade_d_per_index -= max_trade
-        trade_dollars = trade_d_per_index * adj_closing_val_list[i+1]
-        dollars = max_trade * adj_closing_val_list[i+1] + val_per_index * adj_closing_val_list[i+1]
+        trade_dollars = trade_d_per_index * adj_closing_val_list[i+2]
+        dollars = max_trade * adj_closing_val_list[i+2] + val_per_index * adj_closing_val_list[i+2]
         tot_num_trades += 1
-    elif slope < 0 and val_per_index > max_trade:  #If the slope is negative and there are enough invested dollars, sell
+    elif (slope_b < 0 and slope_a > 0) and val_per_index > max_trade:  #If local max and there are enough invested dollars, sell
         trade_d_per_index += max_trade
-        trade_dollars = trade_d_per_index * adj_closing_val_list[i+1]
-        dollars = val_per_index * adj_closing_val_list[i+1] - max_trade * adj_closing_val_list[i+1]
+        trade_dollars = trade_d_per_index * adj_closing_val_list[i+2]
+        dollars = val_per_index * adj_closing_val_list[i+2] - max_trade * adj_closing_val_list[i+2]
         tot_num_trades += 1
-    val_per_index = dollars/adj_closing_val_list[i+1]
-    trade_d_per_index = trade_dollars/adj_closing_val_list[i+1]
-    max_trade = assumed_trade_units/adj_closing_val_list[i+1]
+    val_per_index = dollars/adj_closing_val_list[i+2]
+    trade_d_per_index = trade_dollars/adj_closing_val_list[i+2]
+    max_trade = assumed_trade_units/adj_closing_val_list[i+2]
     
 
 print("You started your investment with $" + str(init_dollars) + '.', end='\n\n')
@@ -68,4 +71,8 @@ print("The trade bin from which you've made changes on your investment has a val
 print("Your total amount of dollars (invested plus trade dollars) after the \
 current trading cycle is ${:.2f} and you traded {} times.".format(
     dollars + trade_dollars, tot_num_trades)
+    , end='\n\n')
+print("Your total amount of dollars after the \
+current investment cycle would be ${:.2f} with no trading involved.".format(
+    init_dollars*(adj_closing_val_list[-1]/adj_closing_val_list[2]))
     )
---
+++ strategy.py
@@ -0,0 +1,3 @@
+def test():
+    print("this is a test")
+    return 0
